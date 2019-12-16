3db92a9577721a3c30513b6497f9c6bac15b1d27
Dmytro Zarezenko <dmytro.zarezenko@gmail.com>
2019-12-16 19:17:23 +0200

    Simple counter example

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,24 @@
 <template>
   <div id="app">
-    
+    <h1>{{ counter }}</h1>
+    <p>
+      {{ getMessage() }}
+    </p>
+    <button @click="counter++">Increase</button> | <button @click="counter--">Decrease</button>
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      counter:  0,
+    }
+  },
+  methods: {
+    getMessage() {
+      return (this.counter < 5) ? "Меньше 5" : "Больше либо равно 5";
+    },
+  },
+}
+</script>
