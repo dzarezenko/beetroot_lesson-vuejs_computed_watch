11e5d96af990c6b051783ff4da3d43b6dbc848b1
Dmytro Zarezenko <dmytro.zarezenko@gmail.com>
2019-12-16 19:24:39 +0200

    2nd counter added

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,12 @@
     <p>
       {{ getMessage() }}
     </p>
+    <p>
+      {{ counter2 }}
+    </p>
     <button @click="counter++">Increase</button> | <button @click="counter--">Decrease</button>
+    <br />
+    <button @click="counter2++">Increase 2</button>
   </div>
 </template>
 
@@ -12,11 +17,13 @@
 export default {
   data() {
     return {
-      counter:  0,
+      counter: 0,
+      counter2: 0,
     }
   },
   methods: {
     getMessage() {
+      console.log("Method called");
       return (this.counter < 5) ? "Меньше 5" : "Больше либо равно 5";
     },
   },
